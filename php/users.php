8623cb26a2dc808b367324ddee3f5455acf9ad95
faithfulcoronel <39255507+faithfulcoronel@users.noreply.github.com>
2025-09-22 14:49:20 +0800

    Convert POS app to PHP with session-based login

---
+++ php/users.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'manager' => [
+        'username' => 'manager',
+        'password' => '1234',
+    ],
+    'cashier' => [
+        'username' => 'cashier',
+        'password' => '1234',
+    ],
+];
